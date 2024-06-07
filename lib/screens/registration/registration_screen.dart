b208c5b8250f2c63156b78d6ed3ff489dcdf9f66
Ottmore <goghijgo@gmail.com>
2024-06-07 08:12:10 +0300

    Added a registration screen

---
+++ lib/screens/registration/registration_screen.dart
@@ -0,0 +1,264 @@
+import 'package:domian/constants/constants.dart';
+import 'package:domian/model/userProfile.dart';
+import 'package:domian/services/auth_service.dart';
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({super.key});
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+
+  late Box user;
+  late Box userProfile;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _lastNameController.dispose();
+    _firstNameController.dispose();
+    _phoneNumberController.dispose();
+    _dateOfBirthController.dispose();
+
+    Hive.close();
+  }
+
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _dateOfBirthController = TextEditingController();
+
+  Future <void> _register() async {
+    if (_formKey.currentState!.validate()) {
+
+      final user = Hive.box('user');
+      final userProfile = Hive.box('user_profile');
+
+      String email = _emailController.text;
+      String password = _passwordController.text;
+      String lastName = _lastNameController.text;
+      String firstName = _firstNameController.text;
+      String phoneNumber = _phoneNumberController.text;
+      String dateOfBirth = _dateOfBirthController.text;
+
+
+      Map<String, dynamic> authData = await AuthService().signUp(email, password, lastName,firstName, phoneNumber, dateOfBirth);
+
+
+      if (authData['isRegister']) {
+        Map<String, dynamic> authUser = authData['user'].toMap();
+
+        UserProfile userProfileObject = await UserProfileModel().getById(authUser['id']);
+
+        await user.add(authUser);
+        await userProfile.add(userProfileObject.toMap());
+
+        pushToProfile();
+      }
+    }
+  }
+
+  pushToProfile() {
+    Navigator.pushNamed(context, '/user-profile');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Создать аккаунт',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.redAccent,
+                  ),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  'Заполните форму для регистрации',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 32),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Эл. почта*',
+                    labelStyle: TextStyle(color: cdomian),
+                    filled: true,
+                    fillColor: Colors.red[50],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: Colors.redAccent),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Пожалуйста, введите свою эл. почту';
+                    }
+                    if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                      return 'Пожалуйста, введите корректный адрес эл. почты';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    labelText: 'Пароль*',
+                    labelStyle: TextStyle(color: cdomian),
+                    filled: true,
+                    fillColor: Colors.red[50],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: Colors.redAccent),
+                    ),
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Пожалуйста, введите свой пароль';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: _lastNameController,
+                  decoration: InputDecoration(
+                    labelText: 'Фамилия*',
+                    labelStyle: TextStyle(color: cdomian),
+                    filled: true,
+                    fillColor: Colors.red[50],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: Colors.redAccent),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Пожалуйста, введите свою фамилию';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: _firstNameController,
+                  decoration: InputDecoration(
+                    labelText: 'Имя',
+                    labelStyle: TextStyle(color: cdomian),
+                    filled: true,
+                    fillColor: Colors.red[50],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: Colors.redAccent),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: _phoneNumberController,
+                  decoration: InputDecoration(
+                    labelText: 'Номер телефона*',
+                    labelStyle: TextStyle(color: cdomian),
+                    filled: true,
+                    fillColor: Colors.red[50],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: Colors.redAccent),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Пожалуйста, введите свой номер телефона';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: _dateOfBirthController,
+                  decoration: InputDecoration(
+                    labelText: 'Дата рождения',
+                    labelStyle: TextStyle(color: cdomian),
+                    filled: true,
+                    fillColor: Colors.red[50],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: Colors.redAccent),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 32),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: _register,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.redAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                    ),
+                    child: Text(
+                      'Зарегистрироваться',
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
