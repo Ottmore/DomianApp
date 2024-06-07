f2a0727d5302151f449d984d02253d21afcf2762
Ottmore <goghijgo@gmail.com>
2024-06-07 07:41:15 +0300

    Created a file with constants for quick access

---
+++ lib/constants/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+const Color white = Colors.white;
+const Color black = Colors.black;
+const Color red = Colors.red;
+const Color cdomian = Color.fromRGBO(227, 30, 39, 1.0);
+
+
+const double appPadding = 30.0;
