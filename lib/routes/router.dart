08b6cfec8d121c9e6f57b3fa9f17c28b21292e16
Ottmore <goghijgo@gmail.com>
2024-06-18 02:23:46 +0300

    added new routes

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,3 +1,5 @@
+import 'package:domian/screens/search/search_screen.dart';
+import 'package:domian/screens/user/favorite/user_favorite.dart';
 import 'package:flutter/material.dart';
 import 'package:domian/routes/slide_page_route.dart';
 import 'package:domian/screens/home/home_screen.dart';
@@ -6,15 +8,19 @@ import 'package:domian/screens/registration/registration_screen.dart';
 import 'package:domian/screens/reviews/reviews_screen.dart';
 import 'package:domian/screens/user/profile/user_profile.dart';
 import 'package:domian/screens/user/settings/user_settings.dart';
+import 'package:domian/screens/user/admin/add_object/addObject_screen.dart';
 
 Map<String, WidgetBuilder> router() {
   return {
     '/': (BuildContext context) => const HomeScreen(),
+    '/search': (BuildContext context) => const SearchScreen(),
+    '/reviews': (BuildContext context) => const ReviewsScreen(),
+    '/user-profile': (BuildContext context) => const UserProfileScreen(),
+    '/favorites': (BuildContext context) => const UserFavorite(),
+    '/setting': (BuildContext context) => const UserSettingsScreen(),
     '/login': (BuildContext context) => const LoginScreen(),
     '/register': (BuildContext context) => const RegistrationScreen(),
-    '/user-profile': (BuildContext context) => const UserProfileScreen(),
-    '/reviews': (BuildContext context) => const ReviewsScreen(),
-    '/setting': (BuildContext context) => const UserSettingsScreen(),
+    '/add-object': (BuildContext context) => CreateObjectScreen(),
   };
 }
 
