d237ffb057172b4490b25adc8caac57a19ade243
Ottmore <goghijgo@gmail.com>
2024-06-18 02:49:59 +0300

    Added dotenv connection, announcement of new models and changed the color of the circular indicators

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:domian/model/user.dart';
 import 'package:domian/model/userProfile.dart';
+import 'package:domian/model/favorite.dart';
 import 'package:domian/routes/router.dart';
-import 'package:domian/routes/slide_page_route.dart';
 import 'package:flutter/material.dart';
 import 'package:domian/constants/constants.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/adapters.dart';
 
 Future<void> main() async {
+  await dotenv.load();
+
   await Hive.initFlutter();
   Hive.registerAdapter(UserAdapter());
   Hive.registerAdapter(UserProfileAdapter());
+  Hive.registerAdapter(FavoriteAdapter());
   await Hive.openBox('user');
   await Hive.openBox('user_profile');
+  await Hive.openBox('favorites');
+  await Hive.openBox('city');
 
   runApp(const DomianApp());
 }
@@ -30,9 +36,12 @@ class DomianApp extends StatelessWidget {
       title: '',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          primarySwatch: Colors.blue,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
           visualDensity: VisualDensity.adaptivePlatformDensity,
           scaffoldBackgroundColor: white,
+          progressIndicatorTheme: ProgressIndicatorThemeData(
+          color: Colors.redAccent,
+          )
       ),
       initialRoute: '/',
       routes: router(),
