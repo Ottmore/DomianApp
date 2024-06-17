293f613dc1d79df0b84b46de8c0d763c744a9b99
Ottmore <goghijgo@gmail.com>
2024-06-18 02:37:12 +0300

    Added image upload via sftp to the slider

--- lib/screens/details/components/carousel_images.dart
+++ lib/screens/details/components/carousel_images.dart
@@ -1,35 +1,58 @@
 import 'package:another_carousel_pro/another_carousel_pro.dart';
+import 'package:domian/services/object_service.dart';
 import 'package:flutter/material.dart';
 
 class CarouselImages extends StatefulWidget {
+  CarouselImages({super.key, required this.objectId, required this.imagesListUrl});
 
+  final int objectId;
   final List<String> imagesListUrl;
-
-  CarouselImages(this.imagesListUrl, {super.key});
 
   @override
   _CarouselImagesState createState() => _CarouselImagesState();
 }
 
 class _CarouselImagesState extends State<CarouselImages> {
+  bool _isLoading = true;
+
+  List<ImageProvider> images = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _loadData();
+  }
+
+  _loadData() async {
+    for (String imageUrl in widget.imagesListUrl) {
+      var image = await ObjectService().getObjectImage(widget.objectId, imageUrl);
+
+      images.add(MemoryImage(image));
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    List<AssetImage> images = [];
-
-    for (String imageUrl in widget.imagesListUrl) {
-      images.add(AssetImage('assets/images/$imageUrl'));
-    }
-
     return SizedBox(
       height: size.height * 0.35,
-      child: AnotherCarousel(
-        dotSize: 5,
-        dotBgColor: Colors.transparent,
-        autoplay: false,
-        images: images,
-      ),
+      child: _isLoading == false
+          ? AnotherCarousel(
+              dotSize: 5,
+              dotBgColor: Colors.transparent,
+              autoplay: false,
+              images: images,
+            )
+          : Container(
+              alignment: Alignment.center,
+              child: const CircularProgressIndicator(),
+            )
     );
   }
 }
