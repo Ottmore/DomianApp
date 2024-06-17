3e6d22e9e6f4f4a8498e12b9b090564138736e32
Ottmore <goghijgo@gmail.com>
2024-06-18 02:33:01 +0300

    Announced a system for filtering objects by city and category

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,9 +3,28 @@ import 'package:flutter/material.dart';
 import 'package:domian/widgets/categories.dart';
 import 'package:domian/screens/home/components/home_app_bar.dart';
 import 'package:domian/widgets/objects_widgets.dart';
+import 'package:hive/hive.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  _HomeScreen createState() => _HomeScreen();
+}
+
+class _HomeScreen extends State<HomeScreen> {
+  String _category = "";
+  final Box _cityBox = Hive.box('city');
+  late String _city = _cityBox.containsKey(0) ? _cityBox.getAt(0) : 'Ростов-на-Дону';
+
+  void updateCategory(String category) => setState((){_category = category;});
+  void updateCity(String city) {
+    _cityBox.put(0, city);
+
+    setState(() {
+      _city = city;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,9 +34,17 @@ class HomeScreen extends StatelessWidget {
         children: [
           Column(
             children: [
-              CustomAppBar(),
-              Categories(),
-              const Houses(),
+              CustomAppBar(
+                city: _city,
+                updateCity: updateCity
+              ),
+              Categories(
+                updateCategory: updateCategory
+              ),
+              Houses(
+                city: _city,
+                category: _category,
+              ),
             ],
           ),
         ],
